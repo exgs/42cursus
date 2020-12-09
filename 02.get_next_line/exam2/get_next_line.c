b75648a2a4488a5fa5ec10b2867f3a7987a63c8b
lielie5 <50637332+lielie5@users.noreply.github.com>
2020-12-09 16:23:52 +0900

    exam2_gnl

---
+++ 02.get_next_line/exam2/get_next_line.c
@@ -0,0 +1,68 @@
+#include "get_next_line.h"
+
+int check_read_before(char **remain, char **line)
+{
+	char *remain_temp;
+
+	if (change_newline_to_zero(*remain) == 1)
+	{
+		remain_temp = *remain;
+		*line = ft_strdup(*remain);
+		*remain = ft_strdup(ft_strchr(*remain, 0) + 1);
+		free(remain_temp);
+		return (1);
+	}
+	else
+		return (0);
+}
+
+int change_newline_to_zero(char *remain)
+{
+	int i;
+
+	i = 0;
+	while (remain[i])
+	{
+		if (remain[i] == '\n')
+		{
+			remain[i] = '\0';
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+int get_next_line_initial_error(int fd, char **line)
+{
+	char buffer[1];
+	if (fd < 0 || BUFFER_SIZE <= 0 || line == NULL || read(fd, buffer, 0) < 0)
+		return (0);
+	return (1);
+}
+
+int get_next_line(int fd, char **line)
+{
+	int ret;
+	char buffer[BUFFER_SIZE + 1];
+	static char *remain;
+	char *remain_temp;
+
+	if (get_next_line_initial_error(fd, line) == 0)
+		return (-1);
+	if (remain != NULL && check_read_before(&remain, line) == 1)
+	// remain이 NULL이면, check_read_before를 실행시키지않음, 실행시키면, newline to zero에서 segfault
+		return (1);
+	ft_memset(buffer, 0, BUFFER_SIZE + 1);
+	while ((ret = read(fd, buffer, BUFFER_SIZE)) > 0)
+	{
+		remain_temp = remain;
+		remain = ft_strjoin(remain, buffer);
+		free(remain_temp);
+		if (check_read_before(&remain, line) == 1)
+			return (1);
+		ft_memset(buffer, 0, BUFFER_SIZE + 1);
+	}
+	*line = remain;
+	return (0);
+}
---
+++ 02.get_next_line/exam2/get_next_line_utils.c
@@ -0,0 +1,96 @@
+#include "get_next_line.h"
+
+int ft_strlen(char *str)
+{
+	int i;
+	
+	i = 0;
+	if (str == NULL)
+		return (-1);
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char *ft_strdup(char *str) //str==NULL이여도, 빈문자열 반환
+{
+	char	*dup;
+	int		len;
+	int		i;
+	
+	i = 0;
+	if (str == NULL)
+	{
+		dup = malloc(1);
+		dup[0] = '\0';
+		return (dup);
+	}
+	len = ft_strlen(str);
+	dup = malloc(sizeof(char) * (len + 1));
+	while (i < len)
+	{
+		dup[i] = str[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
+
+void ft_memset(char *memory, int c, int size)
+{
+	int i;
+
+	i = 0;
+	if (memory == NULL)
+		return;
+	while (i < size)
+	{
+		memory[i] = c;
+		i++;
+	}
+	return ;
+}
+
+char *ft_strchr(char *str, char c)
+{
+	int i;
+	
+	i = 0;
+	if (str == NULL)
+		return (NULL);
+	while (str[i])
+	{
+		if (str[i] == c)
+			return (str + i);
+		i++;
+	}
+	if (c == '\0')
+		return (str + i);
+	else
+		return (NULL);
+}
+
+char *ft_strjoin(char *head, char *tail)
+{
+	char *join;
+	int len;
+	int i;
+
+	if (head == NULL && tail == NULL)
+		return (NULL);
+	else if (head == NULL)
+		return (ft_strdup(tail));
+	else if (tail == NULL)
+		return (ft_strdup(head));
+	i = 0;
+	len = ft_strlen(head) + ft_strlen(tail);
+	join = malloc(sizeof(char) * (len + 1));
+	while (i < len)
+	{
+		if (i < ft_strlen(head))
+			join[i] = head[i];
+		else
+			join[i] = tail[i];
+	}
+	return (join);
+}
