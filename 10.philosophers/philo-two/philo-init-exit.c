96ba17c6e87fec73b748620125ca7a99bb424bc1
Yunsung Lee <yunslee@c6r6s6.42seoul.kr>
2021-03-06 02:19:50 +0900

    philosopher two 새로 업로드

---
+++ 10.philosophers/philo-two/philo-init-exit.c
@@ -0,0 +1,78 @@
+#include "philo_two.h"
+
+int set_info_argv(t_info *info, int argc, char *argv[])
+{
+	if (!(argc == 5 || argc == 6))
+		return (-1);
+	// 음수나 zero 일때, 처리를 해줘야함.
+	info->number_of_philosophers = atoi(argv[1]); // 철학자 수
+	info->time_to_die = atoi(argv[2]); // 철학자의 라이프타임
+	info->time_to_eat = atoi(argv[3]); // 먹는데 걸리는 시간
+	info->time_to_sleep = atoi(argv[4]); // 자는 시간
+	if (argv[5] != NULL)
+		info->meal_full = atoi(argv[5]); // 이정도 묵었으면 마이 묵었다.
+	else
+		info->meal_full = 0;
+}
+
+void semaphore_fork_init(t_info *info)
+{
+	if (0 == (info->forks = 
+				sem_open("forks", O_CREAT, 0755, g_philo_num / 2)))
+		exit(1);
+}
+
+void semaphore_init(t_info *info)
+{
+	semaphore_fork_init(info);
+	if (0 == (info->print_sema = sem_open("print_sema", O_CREAT, 0755, 1)))
+		exit(1);
+}
+
+int set_info(t_info *info)
+{
+	info->anyone_dead = FALSE;
+	info->basetime = get_absolute_time();
+}
+
+int set_philos(t_philo *philos, t_info *info)
+{
+	for (size_t i = 0; i < g_philo_num; i++)
+	{
+		unsigned long init_time;
+
+		init_time = get_relative_time();
+		philos[i].whoami = i;
+		philos[i].when_eat = init_time;
+		philos[i].meal_num = 0;
+	}
+}
+
+unsigned long get_absolute_time()
+{
+	struct timeval time;
+	unsigned long milisecond;
+	gettimeofday(&time, NULL);
+	milisecond = time.tv_sec * 1000 + time.tv_usec / 1000;
+	return (milisecond);
+}
+
+unsigned long get_relative_time()
+{
+	struct timeval time;
+	unsigned long milisecond;
+	unsigned long relative_milisecond;
+	gettimeofday(&time, NULL);
+	milisecond = time.tv_sec * 1000 + time.tv_usec / 1000;
+	relative_milisecond = milisecond - g_info.basetime;
+	return (relative_milisecond);
+}
+
+void print_info(t_info *info)
+{
+	printf("%d\n", info->number_of_philosophers);
+	printf("%d\n", info->time_to_die);
+	printf("%d\n", info->time_to_eat);
+	printf("%d\n", info->time_to_sleep);
+	printf("%d\n", info->meal_full);
+}
---
+++ 10.philosophers/philo-two/philo_two.c
@@ -0,0 +1,49 @@
+#include "philo_two.h"
+
+void *test(void *param)	
+{
+	printf("hello : %d\n", (long)param);
+	return (void *)((long)param + 100);
+}
+
+int start(t_philo *philos, t_info *info)
+{
+	int i;
+	
+	i = 0;
+	semaphore_init(info);
+	while (i < g_philo_num)
+	{
+		philos[i].when_eat = get_relative_time();
+		pthread_create(&philos[i].thread, NULL, philo_do, (void *)&philos[i]);
+		// pthread_create(&philos[i].thread, NULL, test, (void *)i); // 테스트 코드
+		// pthread_join(philos[i].thread, NULL); // 여기다가 넣어주면, 순서대로 철학자 한명씩 먹고자고생각하는게 됨. 나머지는 멍때리는 중
+		i++;
+	}
+	i = 0;
+	while (i < g_philo_num)
+		pthread_join(philos[i++].thread, NULL);
+}
+
+int main(int argc, char *argv[])
+{
+	t_philo *philos;
+	sem_unlink("forks");
+	sem_unlink("print_sema");
+	if (-1 == set_info_argv(&g_info, argc, argv))
+	{
+		printf("error\n");
+		return (-1);
+	}
+	set_info(&g_info);
+	g_philo_num = g_info.number_of_philosophers; // 나 편하자고 전역변수 설정
+	philos = malloc(sizeof(t_philo) * g_philo_num);
+	set_philos(philos, &g_info);
+	start(philos, &g_info);
+
+	sem_close(g_info.forks);
+	sem_unlink("forks");
+	sem_close(g_info.print_sema);
+	sem_unlink("print_sema");
+	return (0);
+}
