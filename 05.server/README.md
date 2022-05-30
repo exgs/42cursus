35feedc03f36be8362b0ffedea89aa1a3ccdac30
yunslee <yunslee@student.42seoul.kr>
2022-05-30 19:55:44 +0900

    Fix: nginx doesn't start when i made ssl rsa:1024
    ref: https://github.com/debauchee/barrier/issues/126
    
    Upload REAME.md

---
+++ 05.server/README.md
@@ -0,0 +1,4 @@
+# How to start
+`{}` 안에 내용은 원하는대로 채워넣기
+1. `docker build -t {imageName} .`
+2. `docker run -it -p 80:80 --name={containerName} {imageName} /bin/bash`
--- 05.server/src/service_start.sh
+++ 05.server/src/service_start.sh
@@ -3,7 +3,7 @@
 
 service mysql start
 
-openssl req -x509 -nodes -days 365 -newkey rsa:1024 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt << EOF
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt << EOF
 KO
 Seoul
 Seoul City
@@ -12,7 +12,7 @@ innovation academy
 yunslee
 yunslee@naver.com
 EOF
-openssl dhparam -out /etc/ssl/certs/dhparam.pem 1024
+openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
 
 #Setting root password
 mysql_secure_installation << EOF
